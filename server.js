d521fdacdb1f6a70e9439596f01438f9087bc836
unknown <Luiz Kischel@NA903450SCIBNU.gruposc.local>
2021-12-13 20:01:19 -0300

    add: service config

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const cors = require("cors");
 const usersRoute = require("./routes/usersRoute");
+const configsRoute = require("./routes/configRoute");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(usersRoute);
+app.use(configsRoute);
 
 app.get('/', (req, res) => {
     return res.json({message:'SERVER UP!'})
