755ab5267a2869312aa3c5dcf7c452ce62e9caed
unknown <Luiz Kischel@NA903450SCIBNU.gruposc.local>
2021-12-13 17:58:25 -0300

    add: teste server

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use(cors());
 app.use(usersRoute);
 
+app.get('/', (req, res) => {
+    return res.json({message:'SERVER UP!'})
+})
+
 app.listen(3333);
 console.log('Server rodando!');
 
